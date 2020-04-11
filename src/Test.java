37ea2e7628229ad95f569a92ca6fd0c447dc5bf1
Gabriel Baptistucci Garcia <gabrielgarcia.8700@aluno.saojuda>
2020-04-11 02:10:32 -0300

    Homework finished

--- src/Test.java
+++ src/Test.java
@@ -3,15 +3,23 @@ import java.util.Arrays;
 public class Test {
 
 	public static void main(String[] args) {
+//		int vet[] = new int[100];
+//		for (int i = 0; i < 100; i++) vet[i] = (int) (Math.random() * 100);
+//		
+//		System.out.println(Arrays.toString(vet));
+//		ArraySort sort = new ArraySort(vet);
+//		sort.mergeSort();
+//		vet = sort.a;
+//		System.out.println(Arrays.toString(vet));
+		
+		
 		int vet[] = new int[100];
 		for (int i = 0; i < 100; i++) vet[i] = (int) (Math.random() * 100);
 		
 		System.out.println(Arrays.toString(vet));
-		ArraySort sort = new ArraySort(vet);
-		sort.mergeSort();
-		vet = sort.a;
+		ArraySort.sorting(vet, 0, vet.length - 1);
 		System.out.println(Arrays.toString(vet));
-
+		
 	}
 
 }
